81cd8069454c17a5f3569e9d92e6530768522db9
JermaineZhao <zx2004@stanford.edu>
2025-05-15 07:12:28 +0100

    lab_6

--- lab_7.py
+++ lab_7.py
@@ -9,10 +9,10 @@ IMAGE_WIDTH = 1400
 
 # TODO: Add your new constants here
 
-TIMEOUT = pass #TODO threshold in timer_callback
-SEARCH_YAW_VEL = pass #TODO searching constant
-TRACK_FORWARD_VEL = pass #TODO tracking constant
-KP = pass #TODO proportional gain for tracking
+TIMEOUT = 2.0 #TODO threshold in timer_callback
+SEARCH_YAW_VEL = 0.5 #TODO searching constant
+TRACK_FORWARD_VEL = 0.5 #TODO tracking constant
+KP = 1.0 #TODO proportional gain for tracking
 
 class State(Enum):
     SEARCH = 0
@@ -39,21 +39,60 @@ class StateMachineNode(Node):
         self.state = State.TRACK
 
         # TODO: Add your new member variables here
-        self.kp = pass # TODO
+        self.kp = KP # TODO
+        self.closest_normalized_bbox_x = 0.0
+        self.last_detect_time = None
+        self.detection_in_progress = False 
 
     def detection_callback(self, msg):
         """
         Determine which of the HAILO detections is the most central detected object
         """
-        pass # TODO: Part 1
+        # TODO: Part 1
+        # breakpoint()
+        most_centered_x = None
+        # dist_to_center = 100
+        # for detection in msg.detections:
+        #     bbox_x = detection.bbox.center.position.x
+        # # print(bbox_x)
+        #     normalized_bbox_x = (bbox_x - IMAGE_WIDTH/2) / (IMAGE_WIDTH/2)
+        #     # print(normalized_bbox_x)
+        #     if abs(normalized_bbox_x) < dist_to_center:
+        #         dist_to_center = abs(normalized_bbox_x)
+        #         most_centered_x = normalized_bbox_x
+        #         self.closest_mormalized_bbox_x = normalized_bbox_x
+        # self.last_detect_time = self.timer.clock.now()
+        # print(self.closest_normalized_bbox_x)
+
+        dist_to_center = 10000
+        self.detection_in_progress = True
+        print("BB")
+        for detection in msg.detections:
+            bbox_x = detection.bbox.center.position.x
+            normalized_bbox_x = (bbox_x - IMAGE_WIDTH / 2) / (IMAGE_WIDTH / 2)
+            print("NBX:", normalized_bbox_x)
+            # Find the closest detection to the center
+            if abs(normalized_bbox_x) < dist_to_center:
+                print("aAAAAAAAAAA")
+                dist_to_center = abs(normalized_bbox_x)
+                self.closest_normalized_bbox_x = normalized_bbox_x
+        
+        self.last_detect_time = self.timer.clock.now()
+        print(self.closest_normalized_bbox_x)
+
+        # breakpoint()
+
 
     def timer_callback(self):
         """
         Implement a timer callback that sets the moves through the state machine based on if the time since the last detection is above a threshold TIMEOUT
         """
-        
-        if False: # TODO: Part 3.2
-            self.state = State.SEARCH
+        time_since_last_detection = (self.get_clock().now() - self.last_detect_time).seconds_nanoseconds()[0]
+        if time_since_last_detection > TIMEOUT: # TODO: Part 3.2
+            if self.detection_in_progress:
+                self.state = State.TRACK
+            else:
+                self.state = State.SEARCH
         else:
             self.state = State.TRACK
 
@@ -61,9 +100,13 @@ class StateMachineNode(Node):
         forward_vel_command = 0.0
 
         if self.state == State.SEARCH:
-            pass # TODO: Part 3.1
+            yaw_command = SEARCH_YAW_VEL # TODO: Part 3.1
+            forward_vel_command = 0.0
         elif self.state == State.TRACK:
-            pass # TODO: Part 2 / 3.4
+            # TODO: Part 2 / 3.4
+            yaw_command = -self.closest_normalized_bbox_x * self.kp
+            forward_vel_command = TRACK_FORWARD_VEL
+            print("yaw: ", yaw_command, "next: ", self.closest_normalized_bbox_x)
 
         cmd = Twist()
         cmd.angular.z = yaw_command
